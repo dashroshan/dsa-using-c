0ca4513cbb11665dcb59fa1e458a189664428882
Roshan Dash <roshan1337d@gmail.com>
2022-07-20 16:02:29 +0530

    lab 9

---
+++ ds lab/Lab9 20-07-22/01 array deque.c
@@ -0,0 +1,213 @@
+// WAP to implement double ended queue using array
+
+#include <stdio.h>
+#define MAX 5
+
+int deque_arr[MAX];
+int l = -1;
+int r = -1;
+
+void insert_r()
+{
+    int added_item;
+    if ((l == 0 && r == MAX - 1) || (l == r + 1))
+    {
+        printf("Queue Overflow\n");
+        return;
+    }
+    if (l == -1)
+    {
+        l = 0;
+        r = 0;
+    }
+    else if (r == MAX - 1)
+        r = 0;
+    else
+        r = r + 1;
+    printf("Input the element for adding in queue : ");
+    scanf("%d", &added_item);
+    deque_arr[r] = added_item;
+}
+
+void insert_l()
+{
+    int added_item;
+    if ((l == 0 && r == MAX - 1) || (l == r + 1))
+    {
+        printf("Queue Overflow \n");
+        return;
+    }
+    if (l == -1)
+    {
+        l = 0;
+        r = 0;
+    }
+    else if (l == 0)
+        l = MAX - 1;
+    else
+        l = l - 1;
+    printf("Input the element for adding in queue : ");
+    scanf("%d", &added_item);
+    deque_arr[l] = added_item;
+}
+
+void delete_l()
+{
+    if (l == -1)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+    printf("Element deleted from queue is : %d\n", deque_arr[l]);
+    if (l == r)
+    {
+        l = -1;
+        r = -1;
+    }
+    else if (l == MAX - 1)
+        l = 0;
+    else
+        l = l + 1;
+}
+
+void delete_r()
+{
+    if (l == -1)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+    printf("Element deleted from queue is : %d\n", deque_arr[r]);
+    if (l == r)
+    {
+        l = -1;
+        r = -1;
+    }
+    else if (r == 0)
+        r = MAX - 1;
+    else
+        r = r - 1;
+}
+
+void display_queue()
+{
+    int front_pos = l, rear_pos = r;
+    if (l == -1)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    printf("Queue elements :\n");
+    if (front_pos <= rear_pos)
+    {
+        while (front_pos <= rear_pos)
+        {
+            printf("%d ", deque_arr[front_pos]);
+            front_pos++;
+        }
+    }
+    else
+    {
+        while (front_pos <= MAX - 1)
+        {
+            printf("%d ", deque_arr[front_pos]);
+            front_pos++;
+        }
+        front_pos = 0;
+        while (front_pos <= rear_pos)
+        {
+            printf("%d ", deque_arr[front_pos]);
+            front_pos++;
+        }
+    }
+    printf("\n");
+}
+
+void input_que()
+{
+    int choice;
+    do
+    {
+        printf("1.Insert at r\n");
+        printf("2.Delete from l\n");
+        printf("3.Delete from r\n");
+        printf("4.Display\n");
+        printf("5.Quit\n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            insert_r();
+            break;
+        case 2:
+            delete_l();
+            break;
+        case 3:
+            delete_r();
+            break;
+        case 4:
+            display_queue();
+            break;
+        case 5:
+            break;
+        default:
+            printf("Wrong choice\n");
+        }
+    } while (choice != 5);
+}
+
+void output_que()
+{
+    int choice;
+    do
+    {
+        printf("1.Insert at r\n");
+        printf("2.Insert at l\n");
+        printf("3.Delete from l\n");
+        printf("4.Display\n");
+        printf("5.Quit\n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert_r();
+            break;
+        case 2:
+            insert_l();
+            break;
+        case 3:
+            delete_l();
+            break;
+        case 4:
+            display_queue();
+            break;
+        case 5:
+            break;
+        default:
+            printf("Wrong choice\n");
+        }
+    } while (choice != 5);
+}
+
+main()
+{
+    int choice;
+    printf("1.Input restricted dequeue\n");
+    printf("2.Output restricted dequeue\n");
+    printf("Enter your choice : ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        input_que();
+        break;
+    case 2:
+        output_que();
+        break;
+    default:
+        printf("Wrong choice\n");
+    }
+}
---
+++ ds lab/Lab9 20-07-22/02 linkedlist deque.c
@@ -0,0 +1,151 @@
+// WAP to implement double ended queue using linkedlist
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *previous, *next;
+};
+
+struct node *head = NULL, *tail = NULL;
+
+struct node *createNode(int data)
+{
+    struct node *nnode = (struct node *)malloc(sizeof(struct node));
+    nnode->data = data;
+    nnode->next = nnode->previous = NULL;
+    return (nnode);
+}
+
+void createSentinels()
+{
+    head = createNode(0);
+    tail = createNode(0);
+    head->next = tail;
+    tail->previous = head;
+}
+
+void enqueueAtFront(int data)
+{
+    struct node *nnode, *temp;
+    nnode = createNode(data);
+    temp = head->next;
+    head->next = nnode;
+    nnode->previous = head;
+    nnode->next = temp;
+    temp->previous = nnode;
+}
+
+void enqueueAtRear(int data)
+{
+    struct node *nnode, *temp;
+    nnode = createNode(data);
+    temp = tail->previous;
+    tail->previous = nnode;
+    nnode->next = tail;
+    nnode->previous = temp;
+    temp->next = nnode;
+}
+
+void dequeueAtFront()
+{
+    struct node *temp;
+    if (head->next == tail)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        temp = head->next;
+        head->next = temp->next;
+        temp->next->previous = head;
+        free(temp);
+    }
+    return;
+}
+
+void dequeueAtRear()
+{
+    struct node *temp;
+    if (tail->previous == head)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        temp = tail->previous;
+        tail->previous = temp->previous;
+        temp->previous->next = tail;
+        free(temp);
+    }
+    return;
+}
+
+void display()
+{
+    struct node *temp;
+
+    if (head->next == tail)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    temp = head->next;
+    while (temp != tail)
+    {
+        printf("%-3d", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int data, ch;
+    createSentinels();
+    while (1)
+    {
+        printf("1. Enqueue at front\n2. Enqueue at rear\n");
+        printf("3. Dequeue at front\n4. Dequeue at rear\n");
+        printf("5. Display\n6. Exit\n");
+        printf("Enter your choice:");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("Enter the data to insert:");
+            scanf("%\nd", &data);
+            enqueueAtFront(data);
+            break;
+
+        case 2:
+            printf("Enter your data to insert:");
+            scanf("\n%d", &data);
+            enqueueAtRear(data);
+            break;
+
+        case 3:
+            dequeueAtFront();
+            break;
+
+        case 4:
+            dequeueAtRear();
+            break;
+
+        case 5:
+            display();
+            break;
+
+        case 6:
+            exit(0);
+
+        default:
+            printf("Plsease enter correct option\n");
+            break;
+        }
+    }
+    return 0;
+}
